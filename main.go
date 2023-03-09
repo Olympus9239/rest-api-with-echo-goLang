adbec2fac1f51afa63751598ed6cf64586634bd4
Amish <ad9187@srmist.edu.in>
2023-03-10 02:38:36 +0530

    put_request_and_delete_request

--- main.go
+++ main.go
@@ -36,15 +36,15 @@ func main() {
 	})
 	e.GET("/products/:id", func(c echo.Context) error {
 		var product map[int]string
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
 		// range over products slice and get each map like {1:"mobiles"}
 		for _, p := range products {
 			for k := range p {
 
-				pId, err := strconv.Atoi(c.Param("id"))
-				if err != nil {
-					return err
-				}
-				if pId == k {
+				if pID == k {
 					product = p
 				}
 			}
@@ -58,9 +58,68 @@ func main() {
 	// e.GET("/products/:vendor", func(c echo.Context) error {
 	// 	return c.JSON(http.StatusOK, c.Param("vendor"))
 	// })
-	e.GET("/products/:vendor", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, c.QueryParam("olderThan"))
+	// e.GET("/products/:vendor", func(c echo.Context) error {
+	// 	return c.JSON(http.StatusOK, c.QueryParam("olderThan"))
+	// })
+	// DELETE
+	e.DELETE("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		var index int
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
+		// range over products slice and get each map like {1:"mobiles"}
+		for i, p := range products {
+			for k := range p {
+				if pID == k {
+					product = p
+					index = i
+				}
+			}
+		}
+		if product == nil {
+			// status not found is 404
+			return c.JSON(http.StatusNotFound, "product not found")
+		}
+		splice := func(s []map[int]string, index int) []map[int]string {
+			return append(s[:index], s[index+1:]...)
+		}
+		products = splice(products, index)
+		return c.JSON(http.StatusOK, product)
 	})
+	e.PUT("/products/:id", func(c echo.Context) error {
+		var product map[int]string
+		pID, err := strconv.Atoi(c.Param("id"))
+		if err != nil {
+			return err
+		}
+		for _, p := range products {
+			for k := range p {
+				if pID == k {
+					product = p
+				}
+			}
+
+		}
+		if product == nil {
+			return c.JSON(http.StatusNotFound, "product not found ")
+		}
+		type body struct {
+			Name string `json:"product_name" validate:"required,min=4"`
+		}
+		var reqBody body
+		e.Validator = &ProdcutValidator{validator: v}
+		if err := c.Bind(&reqBody); err != nil {
+			return err
+		}
+		if err := c.Validate(reqBody); err != nil {
+			return err
+		}
+		product[pID] = reqBody.Name
+		return c.JSON(http.StatusOK, product)
+	})
+
 	e.POST("/products", func(c echo.Context) error {
 		type body struct {
 			Name string `json:"product_name" validate:"required,min=4"` //validate added
